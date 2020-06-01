903066f2cb321872272e8908ad27b7fc7098c79a
imnotberg <aaron.m.weisberg@gmail.com>
2020-06-01 14:19:02 -0400

    del venv

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\aaron\documents\vcm\customer_portal-vcm\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
